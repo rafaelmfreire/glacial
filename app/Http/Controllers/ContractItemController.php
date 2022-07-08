324914fe4854a9586db8d36bdd43abac223759fd
Rafael de Macêdo <r4faelmf@gmail.com>
2022-07-07 22:57:44 -0300

    add base models, controllers and migrations

---
+++ app/Http/Controllers/ContractItemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ContractItem;
+use Illuminate\Http\Request;
+
+class ContractItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ContractItem  $contractItem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ContractItem $contractItem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ContractItem  $contractItem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ContractItem $contractItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ContractItem  $contractItem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ContractItem $contractItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ContractItem  $contractItem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ContractItem $contractItem)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ServiceOrderController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ServiceOrder;
+use Illuminate\Http\Request;
+
+class ServiceOrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ServiceOrder  $serviceOrder
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ServiceOrder $serviceOrder)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ServiceOrder  $serviceOrder
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ServiceOrder $serviceOrder)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ServiceOrder  $serviceOrder
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ServiceOrder $serviceOrder)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ServiceOrder  $serviceOrder
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ServiceOrder $serviceOrder)
+    {
+        //
+    }
+}
