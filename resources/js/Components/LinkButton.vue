4f7ca3fcfe3f43c3a69ffc107cef2d03c9c66a3b
Rafael de Macêdo <r4faelmf@gmail.com>
2022-07-12 21:34:48 -0300

    style: add outline style

--- resources/js/Components/LinkButton.vue
+++ resources/js/Components/LinkButton.vue
@@ -47,7 +47,7 @@ export default {
         // save
         label = this.$slots.default ? this.$slots.default() : 'Salvar';
         classes =
-          'inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white hover:text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-700 active:bg-blue-700 transition duration-150 ease-in-out';
+          'inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white hover:text-white bg-blue-600 hover:bg-blue-500 focus:outline focus:outline-2 focus:outline-blue-500 focus:shadow-outline-blue focus:border-blue-700 active:bg-blue-700 transition duration-150 ease-in-out';
     }
     return h(
       props.tag,
