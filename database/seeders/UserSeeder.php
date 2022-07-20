666f05cca8dcd3608f5205d122e42b7f6b9a1256
Rafael de Macêdo <r4faelmf@gmail.com>
2022-07-20 13:21:47 -0300

    db: add second user seed sample

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -29,5 +29,23 @@ class UserSeeder extends Seeder
             'user_id' => 1, //Rafael
             'personal_team' => true,
         ]);
+
+        DB::table('users')->insert([
+            'name' => 'Nilka',
+            'email' => 'nilkakelly.ufrn@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        DB::table('team_user')->insert([
+            'team_id' => '1', //Assessoria
+            'user_id' => 2, //Nilka
+            'role' => 'admin',
+        ]);
+
+        DB::table('users')->update([
+            'current_team_id' => 1
+        ]);
     }
 }
