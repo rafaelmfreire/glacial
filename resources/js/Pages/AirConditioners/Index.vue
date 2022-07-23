9a99dddbcdd55ab393674632a2b7ecdd6e620a7c
Rafael de Macêdo <r4faelmf@gmail.com>
2022-07-23 08:06:26 -0300

    fix: remove unecessary comments

--- resources/js/Pages/AirConditioners/Index.vue
+++ resources/js/Pages/AirConditioners/Index.vue
@@ -2,7 +2,6 @@
   <AppLayout title="Ares Condionados">
     <template #header>
       <div class="flex items-center justify-between">
-        <!-- <h2 class="font-semibold text-xl text-gray-800 leading-tight">Ares Condicionados</h2> -->
         <Link
           class="
             flex items-center gap-2
